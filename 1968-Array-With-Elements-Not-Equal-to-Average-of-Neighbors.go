360ace3e25c060aaeec6a7b999f3e8a51f66b583
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2024-09-10 17:02:32 +0530

    [1968] [Time Beats: 30.56%] [Memory Beats: 75.00%] - LeetPush

---
+++ 1968-Array-With-Elements-Not-Equal-to-Average-of-Neighbors.go
@@ -0,0 +1,10 @@
+func rearrangeArray(nums []int) []int {
+    sort.Ints(nums)
+    
+    for i := 1; i < len(nums) - 1; i++ {
+        if nums[i - 1] < nums[i] && nums[i] < nums[i + 1] {
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        }
+    }
+    return nums
+}
