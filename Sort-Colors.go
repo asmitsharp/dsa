c4b888c5dbd6277bc456a285165c9cd75b513395
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-05-29 11:29:20 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 11.31%] - LeetPush

---
+++ Sort-Colors.go
@@ -0,0 +1,13 @@
+func sortColors(nums []int)  {
+   for i := 0; i < len(nums) - 1; i++ {
+        minIndex := i
+        for j := i + 1; j < len(nums); j++ {
+            if nums[minIndex] > nums[j] {
+                minIndex = j
+            }
+        }
+
+        nums[i], nums[minIndex] = nums[minIndex], nums[i]
+    } 
+}
+
