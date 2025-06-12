734232c4b22e08d8885253c5326108da78e72f28
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-06-12 09:28:44 +0530

    [128] [Time Beats: 59.97%] [Memory Beats: 55.41%] - LeetPush

---
+++ Longest-Consecutive-Sequence.go
@@ -0,0 +1,30 @@
+func longestConsecutive(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    
+    numSet := make(map[int]bool)
+    for _, num := range nums {
+        numSet[num] = true
+    }
+    
+    maxLength := 0
+    
+    for num := range numSet {
+        if !numSet[num-1] {
+            currentNum := num
+            currentLength := 1
+
+            for numSet[currentNum+1] {
+                currentNum++
+                currentLength++
+            }
+            
+            if currentLength > maxLength {
+                maxLength = currentLength
+            }
+        }
+    }
+    
+    return maxLength
+}
