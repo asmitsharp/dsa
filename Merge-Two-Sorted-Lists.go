6bbc3e509a2634e16f69f56c5188af4451f2d5ba
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-09-04 18:08:17 +0530

    [21] [Time Beats: 100.00%] [Memory Beats: 39.68%] - LeetPush

---
+++ Merge-Two-Sorted-Lists.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    return merge(list1, list2)
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    tail := dummy
+
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
+            tail.Next = l1
+            l1 = l1.Next
+        } else {
+            tail.Next = l2
+            l2 = l2.Next
+        }
+        tail = tail.Next
+    }
+
+    if l1 != nil {
+        tail.Next = l1
+    } else {
+        tail.Next = l2
+    }
+
+    return dummy.Next
+}
