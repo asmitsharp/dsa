211d1c99b6b80099410bd6b7d89a51094f81bb1b
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-09-04 19:16:13 +0530

    [350] [Time Beats: 100.00%] [Memory Beats: 85.07%] - LeetPush

---
+++ Intersection-of-Two-Arrays-II.go
@@ -0,0 +1,22 @@
+import "sort"
+func intersect(nums1 []int, nums2 []int) []int {
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+
+    i, j := 0, 0
+    res := []int{}
+
+    for i < len(nums1) && j < len(nums2) {
+        if nums1[i] == nums2[j] {
+            res = append(res, nums1[i])
+            i++
+            j++
+        } else if nums1[i] > nums2[j] {
+            j++
+        } else {
+            i++
+        }
+    }
+
+    return res
+}
