59c5b3c946c8c81cb04ba6521671ced433c397d2
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-09-05 16:40:52 +0530

    [151] [Time Beats: 100.00%] [Memory Beats: 59.85%] - LeetPush

---
+++ Reverse-Words-in-a-String.go
@@ -0,0 +1,11 @@
+func reverseWords(s string) string {
+    words := strings.Fields(s)
+    result := ""
+
+    for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+        words[i], words[j] = words[j], words[i]
+    }
+
+    result = strings.Join(words, " ")
+    return result
+}
