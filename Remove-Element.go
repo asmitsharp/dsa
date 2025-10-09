9a2c410e81bf739254373ce08de3d30362fc0add
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2025-10-09 12:08:56 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 67.72%] - LeetPush

---
+++ Remove-Element.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+    l, r := 0, len(nums) 
+    for l < r {
+        if nums[l] == val {
+            nums[l] = nums[r - 1]
+            r--
+        } else {
+            l++
+        }
+    }
+
+    return r
+}
