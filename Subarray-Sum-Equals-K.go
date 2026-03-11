70b6ace387185c45c64f4462e8eda44be857fbd8
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-11 10:32:09 +0530

    [560] [Time Beats: 65.55%] [Memory Beats: 80.21%] - LeetPush

--- Subarray-Sum-Equals-K.go
+++ Subarray-Sum-Equals-K.go
@@ -1,19 +1,12 @@
-func subarraySum(nums []int, k int) int {
-    count := 0
-    prefixSum := 0
-
-    prefixCount := map[int]int{0: 1}
-
-    for _, num := range nums {
-        prefixSum += num
-
-        if val, exists := prefixCount[prefixSum - k]; exists {
-            count += val
-        }
-
-        prefixCount[prefixSum]++
-    }
-
-    return count
-}
-
+1func subarraySum(nums []int, k int) int {
+2      count := 0
+3    prefixSum := 0
+4    freq := map[int]int{0: 1}
+5
+6    for _, num := range nums {
+7        prefixSum += num
+8        count += freq[prefixSum-k]
+9        freq[prefixSum]++
+10    }
+11    return count
+12}
