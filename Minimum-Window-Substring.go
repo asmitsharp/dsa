5d08e14ef5e525a4760a75d885478126c57db66a
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-17 13:39:05 +0530

    [76] [Time Beats: 53.85%] [Memory Beats: 93.88%] - LeetPush

--- Minimum-Window-Substring.go
+++ Minimum-Window-Substring.go
@@ -1,52 +1,42 @@
-
-
-func minWindow(s string, t string) string {
-    if len(s) == 0 || len(t) == 0 || len(s) < len(t) {
-        return ""
-    }
-
-    tFreq := make(map[byte]int)
-    for i := 0; i < len(t); i++ {
-        tFreq[t[i]]++
-    }
-
-    required := len(tFreq) 
-    formed := 0           
-    windowFreq := make(map[byte]int)
-    start, minLen := 0, len(s)+1
-    resultStart, resultEnd := 0, 0
-
-    for right := 0; right < len(s); right++ {
-    
-        char := s[right]
-        windowFreq[char]++
-
-        
-        if count, needed := tFreq[char]; needed && windowFreq[char] == count {
-            formed++
-        }
-
-    
-        for left := start; formed == required && left <= right; left++ {
-
-            if right-left+1 < minLen {
-                minLen = right - left + 1
-                resultStart, resultEnd = left, right+1
-            }
-
-           
-            char = s[left]
-            windowFreq[char]--
-            if count, needed := tFreq[char]; needed  && windowFreq[char] < count {
-                formed--
-            }
-            start++
-        }
-    }
-
- 
-    if minLen > len(s) {
-        return ""
-    }
-    return s[resultStart:resultEnd]
-}
+1func minWindow(s string, t string) string {
+2    left := 0
+3    need := make(map[byte]int)
+4    win := make(map[byte]int)
+5    minLen := len(s) + 1
+6    minStart := 0
+7
+8    for i := 0; i < len(t); i++ {
+9        need[t[i]]++
+10    }
+11
+12    var match int
+13    required := len(need)
+14
+15    for right := 0; right < len(s); right++ {
+16        c := s[right]
+17        win[c]++
+18
+19        if win[c] == need[c] {
+20            match++
+21        }
+22
+23        for match == required {
+24            winLen :=  right - left + 1
+25            if winLen < minLen {
+26                minLen = winLen
+27                minStart = left
+28            }
+29
+30            leftChar := s[left]
+31            win[leftChar]--
+32            if win[leftChar] < need[leftChar]{
+33                match--
+34            }
+35            left++
+36        }
+37    }
+38     if minLen == len(s) + 1 {
+39        return ""
+40    }
+41    return s[minStart : minStart+minLen]
+42}
