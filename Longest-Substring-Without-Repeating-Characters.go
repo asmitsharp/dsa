24d701dee7e5bdb2b49d8e56607d745e30a36399
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-17 12:47:13 +0530

    [3] [Time Beats: 75.32%] [Memory Beats: 59.21%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.go
+++ Longest-Substring-Without-Repeating-Characters.go
@@ -1,21 +1,22 @@
-func lengthOfLongestSubstring(s string) int {
-    maxL := 0
-
-    wStart := 0
-    charIndex := make(map[byte]int)
-
-    for i := 0 ; i < len(s); i++ {
-
-        if lastIndex, found := charIndex[s[i]]; found && lastIndex >= wStart {
-            wStart = lastIndex + 1
-        }
-
-        charIndex[s[i]] = i
-        currL := i - wStart + 1
-        if currL > maxL {
-            maxL = currL
-        }
-    }
-
-    return maxL
-}
+1func lengthOfLongestSubstring(s string) int {
+2    char := make(map[byte]int)
+3    left := 0
+4    maxLen := 0
+5
+6    for right := 0; right < len(s); right++ {
+7        char[s[right]]++
+8
+9        for char[s[right]] > 1 {
+10            char[s[left]]--
+11            left++
+12        }
+13
+14        windowLen := right - left + 1
+15
+16        if windowLen > maxLen {
+17            maxLen = windowLen
+18        }
+19    }
+20
+21    return maxLen
+22}
