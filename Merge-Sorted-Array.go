de6b79c09cf76fc43e05e032936cd2e66b7a0a92
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-18 13:32:39 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 78.15%] - LeetPush

--- Merge-Sorted-Array.go
+++ Merge-Sorted-Array.go
@@ -1,14 +1,14 @@
-func merge(nums1 []int, m int, nums2 []int, n int)  {
-    i,j,k := m-1, n-1, m+n-1
-
-    for j >= 0 {
-        if i >= 0 && nums1[i] > nums2[j] {
-            nums1[k] = nums1[i]
-            i--
-        } else {
-            nums1[k] = nums2[j]
-            j--
-        }
-        k--
-    }
-}
+1func merge(nums1 []int, m int, nums2 []int, n int)  {
+2    p1, p2, p := m-1, n-1, m+n-1
+3
+4    for p2 >= 0 {
+5        if p1 >= 0 && nums1[p1] > nums2[p2] {
+6            nums1[p] = nums1[p1]
+7            p1--
+8        } else {
+9            nums1[p] = nums2[p2]
+10            p2--
+11        }
+12        p--
+13    }
+14}
