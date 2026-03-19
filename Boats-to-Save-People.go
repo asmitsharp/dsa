95da13a2486a2be0c75eaac7f8ae8854d47e9ca1
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-19 12:13:12 +0530

    [881] [Time Beats: 77.78%] [Memory Beats: 8.50%] - LeetPush

--- Boats-to-Save-People.go
+++ Boats-to-Save-People.go
@@ -1,15 +1,22 @@
-func numRescueBoats(people []int, limit int) int {
-    sort.Ints(people)
-    l, r := 0, len(people) - 1
-    count := 0
-    for l <= r {
-        if people[l] + people[r]  <= limit {
-            l++
-        } 
-        r--
-        count++
-
-    }
-
-    return count
-}
+1func numRescueBoats(people []int, limit int) int {
+2    sort.Ints(people)
+3    left := 0
+4    right := len(people) - 1
+5    boat := 0
+6
+7    for left <= right {
+8        if left == right{
+9            boat++
+10            break
+11        }
+12        sum := people[left] + people[right] 
+13        if sum <= limit {
+14            left++  
+15            right--
+16        } else if sum > limit {
+17            right--
+18        }
+19        boat++
+20    }
+21    return boat
+22}
