60c3d1d02e9ed3c96fd93a31cf0f0b7fec648027
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-19 11:52:29 +0530

    [977] [Time Beats: 100.00%] [Memory Beats: 35.80%] - LeetPush

--- Squares-of-a-Sorted-Array.go
+++ Squares-of-a-Sorted-Array.go
@@ -1,24 +1,23 @@
-func sortedSquares(nums []int) []int {
-   n := len(nums)
-   result := make([]int, n)
-
-   l := 0
-   r := n - 1
-   p := n - 1
-
-   for l <= r {
-    left := nums[l] * nums[l]
-    right := nums[r] * nums[r]
-
-    if left > right {
-        result[p] = left
-        l++
-    } else {
-        result[p] = right
-        r--
-    }
-    p--
-   }
-
-   return result
-}
+1func sortedSquares(nums []int) []int {
+2    result := make([]int, len(nums))
+3
+4    left := 0
+5    right := len(nums) - 1
+6    pos := len(nums) - 1
+7
+8    for left <= right {
+9        squareLeft := nums[left] * nums[left]
+10        squareRight := nums[right] * nums[right]
+11
+12        if squareLeft < squareRight {
+13            result[pos] = squareRight
+14            right--
+15        } else {
+16            result[pos] = squareLeft
+17            left++
+18        }
+19        pos--
+20    }
+21
+22    return result
+23}
