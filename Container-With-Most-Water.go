bf3467b2a2be5aa65fdc7af50ef16549d3df031a
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-19 10:32:12 +0530

    [11] [Time Beats: 100.00%] [Memory Beats: 87.33%] - LeetPush

--- Container-With-Most-Water.go
+++ Container-With-Most-Water.go
@@ -1,24 +1,22 @@
-func maxArea(height []int) int {
-    left := 0
-    right := len(height) - 1
-
-    max_area := 0
-
-    for left < right {
-        width := right - left
-        curr_area := 0
-        if height[left] < height[right] {
-            curr_area = width * height[left]
-            left++
-        } else {
-            curr_area = width * height[right]
-            right--
-        }
-
-        if curr_area > max_area {
-            max_area = curr_area
-        }
-    }
-
-    return max_area
-}
+1func maxArea(height []int) int {
+2    left := 0
+3    right := len(height) - 1
+4    maxWater := 0
+5
+6    for left < right {
+7        water := 0
+8        if height[left] > height[right] {
+9            water = height[right] * (right - left)
+10            right--
+11        } else {
+12            water = height[left] * (right - left)
+13            left++
+14        }
+15
+16        if water > maxWater {
+17            maxWater = water
+18        }
+19    }
+20
+21    return maxWater
+22}
