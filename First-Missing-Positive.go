5ae258db2cc3d9e625015145bf6ae873d073f2e6
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-03-20 11:07:52 +0530

    [41] [Time Beats: 100.00%] [Memory Beats: 37.47%] - LeetPush

--- First-Missing-Positive.go
+++ First-Missing-Positive.go
@@ -1,31 +1,22 @@
-func firstMissingPositive(nums []int) int {
-    n := len(nums) 
-
-    for i := 0; i < n; i++ {
-        if nums[i] <= 0 || nums[i] > n {
-            nums[i] = n + 1 
-        }
-    }
-
-    for i := 0; i < n; i++ {
-        num := abs(nums[i])
-        if num <= n {
-            nums[num-1] = -abs(nums[num-1]) 
-        }
-    }
-
-    for i := 0; i < n; i++ {
-        if nums[i] > 0 {
-            return i + 1
-        }
-    }
-
-    return n+1
-}
-
-func abs(x int) int {
-    if x < 0 {
-        return -x
-    }
-    return x
-}
+1func firstMissingPositive(nums []int) int {
+2    i := 0
+3    n := len(nums)
+4
+5    for i < n {
+6        if nums[i] > 0 && nums[i] <= n {
+7            correctIdx := nums[i] - 1
+8            if nums[i] != nums[correctIdx] {
+9            nums[i], nums[correctIdx] = nums[correctIdx], nums[i]
+10            continue
+11            } 
+12        }
+13        i++
+14    }
+15
+16    for i := 0; i < n; i++ {
+17        if nums[i] != i+1 {
+18            return i+1
+19        }
+20    }
+21    return n+1
+22}
