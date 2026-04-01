4a02f56887abf2e32b410741b64ce1e920a65e6f
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-04-01 10:13:56 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 28.10%] - LeetPush

--- Search-Insert-Position.go
+++ Search-Insert-Position.go
@@ -1,24 +1,25 @@
-// func searchInsert(nums []int, target int) int {
-//     left, right := 0, len(nums)
-//     for left < right {
-//         mid := left + (right - left) / 2
-//         if target > nums[mid] {
-//             left = mid + 1
-//         } else {
-//             right = mid
-//         } 
-//     }
-//     return left
-// }
-
-
-func searchInsert(nums []int, target int) int {
-   i := 0
-
-   for i < len(nums) && nums[i] < target {
-    i++
-   } 
-
-   return i
-}
-
+1// func searchInsert(nums []int, target int) int {
+2//     left, right := 0, len(nums)
+3//     for left < right {
+4//         mid := left + (right - left) / 2
+5//         if target > nums[mid] {
+6//             left = mid + 1
+7//         } else {
+8//             right = mid
+9//         } 
+10//     }
+11//     return left
+12// }
+13
+14
+15func searchInsert(nums []int, target int) int {
+16   i := 0
+17
+18   for i < len(nums) && nums[i] < target {
+19    i++
+20   } 
+21
+22   return i
+23}
+24
+25
