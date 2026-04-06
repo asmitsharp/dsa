ff8b67dbd1f72f16bd76655ad832d8f6a3f76a42
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-04-06 11:22:26 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 49.87%] - LeetPush

--- Missing-Number.go
+++ Missing-Number.go
@@ -1,19 +1,9 @@
 1func missingNumber(nums []int) int {
-2    i := 0
-3    for i < len(nums) {
-4        correctIdx := nums[i]
-5        if nums[i] < len(nums) && nums[i] != nums[correctIdx] {
-6            nums[i], nums[correctIdx] = nums[correctIdx], nums[i]
-7        } else {
-8            i++
-9        }
-10    }
-11
-12    for i := 0; i < len(nums); i++ {
-13        if nums[i] != i {
-14            return i
-15        }
-16    }
-17
-18    return len(nums)
-19}
+2    res := len(nums)
+3
+4    for i, n := range nums {
+5        res ^= i ^ n
+6    }
+7
+8    return res
+9}
