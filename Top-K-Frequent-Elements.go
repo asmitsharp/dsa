5540113ae879b267cb8acf47b5242db8de4936d7
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-04-09 11:01:16 +0530

    [347] [Time Beats: 27.59%] [Memory Beats: 62.64%] - LeetPush

--- Top-K-Frequent-Elements.go
+++ Top-K-Frequent-Elements.go
@@ -1,101 +1,47 @@
-// func topKFrequent(nums []int, k int) []int {
-//     count := make(map[int]int)
-// \tfor _, num := range nums {
-// \t\tcount[num]++
-// \t}
-
-// \tunique := make([]int, 0, len(count))
-// \tfor key := range count {
-// \t\tunique = append(unique, key)
-// \t}
-
-// \tpartition := func(left, right, pivotIndex int) int {
-// \t\tpivotFrequency := count[unique[pivotIndex]]
-// \t\tunique[pivotIndex], unique[right] = unique[right], unique[pivotIndex]
-
-// \t\tstoreIndex := left
-// \t\tfor i := left; i < right; i++ {
-// \t\t\tif count[unique[i]] < pivotFrequency {
-// \t\t\t\tunique[storeIndex], unique[i] = unique[i], unique[storeIndex]
-// \t\t\t\tstoreIndex++
-// \t\t\t}
-// \t\t}
-
-// \t\tunique[right], unique[storeIndex] = unique[storeIndex], unique[right]
-
-// \t\treturn storeIndex
-// \t}
-
-//     var quickSelect func(int, int, int)
-// \tquickSelect = func(left, right, kSmallest int) {
-// \t\tif left == right {
-// \t\t\treturn
-// \t\t}
-
-// \t\tpivotIndex := rand.Intn(right-left+1) + left
-// \t\tpivotIndex = partition(left, right, pivotIndex)
-
-// \t\tif kSmallest == pivotIndex {
-// \t\t\treturn
-// \t\t} else if kSmallest < pivotIndex {
-// \t\t\tquickSelect(left, pivotIndex-1, kSmallest)
-// \t\t} else {
-// \t\t\tquickSelect(pivotIndex+1, right, kSmallest)
-// \t\t}
-// \t}
-
-// \tn := len(unique)
-// \tquickSelect(0, n-1, n-k)
-
-// \treturn unique[n-k:]
-// }
-
-func topKFrequent(nums []int, k int) []int {
-    freq := make(map[int]int)
-    for _, num := range nums {
-        freq[num]++
-    }
-
-    pairs := make([][2]int, 0, len(freq))
-    for num, f := range freq {
-        pairs = append(pairs, [2]int{f, num})
-    }
-
-    return heapsort(pairs, k)
-}
-
-func heapsort(pairs [][2]int, k int) []int {
-    n := len(pairs)
-
-    for i := n/2 - 1; i >= 0; i-- {
-        heapify(pairs, n, i)
-    }
-
-    result := make([]int, k)
-    for i := 0; i < k; i++ {
-        result[i] = pairs[0][1]
-        pairs[0], pairs[n-1-i] = pairs[n-1-i], pairs[0]
-        heapify(pairs, n-1-i, 0)
-    }
-
-    return result
-}
-
-func heapify(pairs [][2]int, n, i int) {
-    largest := i
-    left := 2*i + 1
-    right := 2*i + 2
-
-    if left < n && pairs[left][0] > pairs[largest][0] {
-        largest = left
-    }
-
-    if right < n && pairs[right][0] > pairs[largest][0] {
-        largest = right
-    }
-
-    if largest != i {
-        pairs[i], pairs[largest] = pairs[largest], pairs[i]
-        heapify(pairs, n, largest)
-    }
-}
+1
+2type Pair struct {
+3    num int
+4    freq int
+5}
+6
+7type MinHeap []Pair
+8
+9func (h MinHeap) Len() int {return len(h)}
+10func (h MinHeap) Less(i, j int) bool {return h[i].freq < h[j].freq}
+11func (h MinHeap) Swap(i, j int) {h[i], h[j] = h[j], h[i]}
+12
+13func (h *MinHeap) Push(x interface{}) {
+14  *h = append(*h, x.(Pair))
+15}
+16
+17func (h *MinHeap) Pop() interface{} {
+18    old := *h
+19    n := len(old)
+20    x := old[n-1]
+21    *h = old[:n-1]
+22
+23    return x
+24}
+25
+26func topKFrequent(nums []int, k int) []int {
+27    freq := map[int]int{}
+28    for _, num := range nums {
+29        freq[num]++
+30    }
+31
+32     h := &MinHeap{}
+33     heap.Init(h)
+34
+35     for num, f := range freq {
+36        heap.Push(h, Pair{num, f})
+37        if h.Len() > k {
+38            heap.Pop(h)
+39        }
+40     }
+41
+42     res := []int{}
+43     for h.Len() > 0 {
+44        res = append(res, heap.Pop(h).(Pair).num)
+45     }
+46     return res
+47}
