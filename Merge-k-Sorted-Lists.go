875488d7e01da7cbc1785edb7fc8149adbfbefed
Asmit Singh <36570900+asmitsharp@users.noreply.github.com>
2026-04-10 12:04:51 +0530

    [23] [Time Beats: 32.76%] [Memory Beats: 60.57%] - LeetPush

--- Merge-k-Sorted-Lists.go
+++ Merge-k-Sorted-Lists.go
@@ -1,56 +1,45 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func mergeKLists(lists []*ListNode) *ListNode {
-    if len(lists) == 0 {
-        return nil
-    } 
-    if len(lists) == 1 {
-        return lists[0]
-    }
-    
-    for len(lists) > 1 {
-        var newLists []*ListNode
-
-        for i := 0; i < len(lists); i += 2 {
-            l1 := lists[i]
-            var l2 *ListNode
-            if (i+1) < len(lists) {
-                l2 = lists[i+1]
-            }
-            merged := merge(l1, l2)
-            newLists = append(newLists, merged)
-        }
-        lists = newLists
-    }
-
-    return lists[0]
-}
-
-func merge(l1, l2 *ListNode) *ListNode {
-    dummy := &ListNode{}
-    tail := dummy
-
-    for l1 != nil && l2 != nil {
-        if l1.Val < l2.Val {
-            tail.Next = l1
-            l1 = l1.Next
-        } else {
-            tail.Next = l2
-            l2 = l2.Next
-        }
-        tail = tail.Next
-    }
-
-    if l1 != nil {
-        tail.Next = l1
-    } else {
-        tail.Next = l2
-    }
-
-    return dummy.Next
-}
+1/**
+2 * Definition for singly-linked list.
+3 * type ListNode struct {
+4 *     Val int
+5 *     Next *ListNode
+6 * }
+7 */
+8 type NodeHeap []*ListNode
+9
+10func (h NodeHeap) Len() int            { return len(h) }
+11func (h NodeHeap) Less(i, j int) bool  { return h[i].Val < h[j].Val }
+12func (h NodeHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+13func (h *NodeHeap) Push(x interface{}) { *h = append(*h, x.(*ListNode)) }
+14func (h *NodeHeap) Pop() interface{} {
+15    old := *h
+16    n := len(old)
+17    node := old[n-1]
+18    *h = old[:n-1]
+19    return node
+20}
+21func mergeKLists(lists []*ListNode) *ListNode {
+22    h := &NodeHeap{}
+23    heap.Init(h)
+24
+25    for _, list := range lists {
+26        if list != nil {
+27            heap.Push(h, list)
+28        }
+29    }
+30
+31    dummy := &ListNode{}
+32    curr := dummy
+33
+34    for h.Len() > 0 {
+35        node := heap.Pop(h).(*ListNode)
+36        curr.Next = node
+37        curr = curr.Next
+38
+39        if node.Next != nil {
+40            heap.Push(h, node.Next)
+41        }
+42    }
+43
+44    return dummy.Next
+45}
